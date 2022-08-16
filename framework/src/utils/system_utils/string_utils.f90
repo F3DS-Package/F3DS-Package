8262dfaa79fcf9cc34d902ac16413c13f81e75cf
Tatsumasa Ishikawa <tatsumasa.ishikawa@ishsoftware.jp>
2022-08-16 16:16:30 +0900

    Add string utils

---
+++ framework/src/utils/system_utils/string_utils.f90
@@ -0,0 +1,43 @@
+module string_utils_module
+    use typedef_module
+
+    implicit none
+
+    private
+
+    public :: to_str
+
+    contains
+
+    pure function to_str(num, extra_digits) result(str)
+        integer  (int_kind), intent(in)           :: num
+        integer  (int_kind), intent(in), optional :: extra_digits
+        character(:       ), allocatable          :: str
+
+        integer  (int_kind)              :: num_digits
+        integer  (int_kind)              :: dgt_digits
+        character(:       ), allocatable :: str_digits
+        character(:       ), allocatable :: fmt
+
+        if(.not. present(extra_digits)) extra_digits = 0
+
+        num_digits = get_digits_of(num) + extra_digits
+        dgt_digits = get_digits_of(num_digits)
+
+        allocate(character(dgt_digits)::str_digits)
+        write(str_digits,'(I0)') num_digits
+
+        fmt = "(I"//str_digits//"."//str_digits//")"
+
+        allocate(character(num_digits)::str)
+        write(str,fmt) num
+    end function
+
+    pure function get_digits_of(num) result(num_digit)
+        integer(int_kind), intent(in) :: num
+        integer(int_kind)             :: num_digit
+
+        num_digit = int(log10(dble(num)))+1
+    end function get_digits_of
+
+end module string_utils_module
