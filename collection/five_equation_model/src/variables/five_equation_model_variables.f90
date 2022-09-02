4d32d9c164a01348b4c026198e762abec5a740bf
Tatsumasa ISHIKAWA <tishikawa@st.go.tuat.ac.jp>
2022-09-02 14:48:08 +0900

    Renew shocktube

--- collection/five_equation_model/src/variables/five_equation_model_variables.f90
+++ collection/five_equation_model/src/variables/five_equation_model_variables.f90
@@ -264,7 +264,7 @@ module five_equation_model_variables_module
             kappa => surface_tension_variables(4)  &
         )
             if((interface_threshold < z) .and. (z < 1.d0 - interface_threshold) .and. abs(kappa) < 1.d0)then
-                dst_primitives(8) = (1.d0 / weber_number) * kappa
+                dst_primitives(8) = 0.d0!(1.d0 / weber_number) * kappa
             else
                 dst_primitives(8) = 0.d0
             endif
