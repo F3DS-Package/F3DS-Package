70969b1d3fb940742d901a22a3c462db863396b4
Tatsumasa Ishikawa <tatsumasa.ishikawa@ishsoftware.jp>
2022-10-11 18:02:04 +0900

    Fix stdio

--- framework/src/utils/system_utils/stdio.f90
+++ framework/src/utils/system_utils/stdio.f90
@@ -1,23 +1,38 @@
 module stdio_module
     implicit none
 
-    public
+    private
+
+    character(len=5) :: color_green = char(27)//'[32m'
+    character(len=5) :: color_yelow = char(27)//'[33m'
+    character(len=5) :: color_red   = char(27)//'[31m'
+    character(len=5) :: color_cyan  = char(27)//'[36m'
+    character(len=4) :: esc_reset   = char(27)//'[0m'
+
+    public :: call_error
+    public :: write_message
+    public :: write_warring
 
     contains
 
     subroutine call_error(message)
         character(len=*) :: message
-        print *, "\033[31m Error\033[m: ", message
+        print *, color_red//"Error: "//esc_reset, message
         stop
     end subroutine call_error
 
     subroutine write_message(message)
         character(len=*) :: message
-        print *, "\033[32m Message\033[m: ", message
+        print *, color_green//"Message: "//esc_reset, message
     end subroutine write_message
 
     subroutine write_warring(message)
         character(len=*) :: message
-        print *, "\033[33m Warning\033[m: ", message
+        print *, color_yelow//"Warning: "//esc_reset, message
     end subroutine write_warring
+
+    subroutine write_debuginfo(message)
+        character(len=*) :: message
+        print *, color_cyan//"Debug: "//esc_reset, message
+    end subroutine write_debuginfo
 end module stdio_module
