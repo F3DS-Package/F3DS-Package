a65c2f9274fe4f06e03c4924808fa9c4f77ffd4d
Tatsumasa Ishikawa <tatsumasa.ishikawa@ishsoftware.jp>
2023-02-10 18:37:26 +0900

    Add face gradient calculator

---
+++ framework/src/face_gradient_calculator/central_difference_face_gradient_calculator.f90
@@ -0,0 +1,44 @@
+module class_central_difference_face_gradient_calculator
+    use typedef_module
+    use abstract_face_gradient_calculator
+    use abstract_configuration
+
+    implicit none
+
+    private
+
+    type, public, extends(face_gradient_calculator) :: central_difference_face_gradient_calculator
+        contains
+        procedure, public, pass(self) :: initialize
+        procedure, public, pass(self) :: compute
+    end type
+
+    contains
+
+    subroutine initialize(self, config)
+        class(central_difference_face_gradient_calculator), intent(inout) :: self
+        class(configuration                              ), intent(inout) :: config
+    end subroutine initialize
+
+    pure function compute(self, lhc_variables, rhc_variables, lhc_position, rhc_position, num_variables) result(gradient_variables)
+        class  (central_difference_face_gradient_calculator), intent(in) :: self
+        real   (real_kind                                  ), intent(in) :: lhc_variables(:)
+        real   (real_kind                                  ), intent(in) :: rhc_variables(:)
+        real   (real_kind                                  ), intent(in) :: lhc_position (3)
+        real   (real_kind                                  ), intent(in) :: rhc_position (3)
+        integer(int_kind                                   ), intent(in) :: num_variables
+        real   (real_kind                                  )             :: gradient_variables(num_variables*3)
+
+        integer(int_kind  ) :: i
+
+        do i = 1, num_variables, 1
+            associate(                                           &
+                grad => gradient_variables(3*(i-1)+1:3*(i-1)+3), &
+                dphi => lhc_variables(i) - rhc_variables(i)    , &
+                dx   => lhc_position - rhc_position              &
+            )
+                grad(1:3) = dphi / dx
+            end associate
+        end do
+    end function compute
+end module class_central_difference_face_gradient_calculator
