f4ab0f4edfce58116cf275a44006c1b0058fb407
Tatsumasa Ishikawa <tatsumasa.ishikawa@ishsoftware.jp>
2023-02-16 15:40:50 +0900

    Stage abstract

---
+++ framework/src/abstract/core/abstract_face_gradient_calculator.f90
@@ -0,0 +1,34 @@
+module abstract_face_gradient_calculator
+    implicit none
+
+    private
+
+    type, public, abstract :: face_gradient_calculator
+        contains
+        procedure(initialize_interface), pass(self), deferred :: initialize
+        procedure(compute_interface   ), pass(self), deferred :: compute
+    end type
+
+    abstract interface
+        subroutine initialize_interface(self, config)
+            use abstract_configuration
+            import face_gradient_calculator
+
+            class(face_gradient_calculator), intent(inout) :: self
+            class(configuration           ), intent(inout) :: config
+        end subroutine initialize_interface
+
+        pure function compute_interface(self, lhc_variables, rhc_variables, lhc_position, rhc_position, num_variables) result(gradient_variables)
+            use typedef_module
+            import face_gradient_calculator
+
+            class  (face_gradient_calculator), intent(in) :: self
+            real   (real_kind               ), intent(in) :: lhc_variables(:)
+            real   (real_kind               ), intent(in) :: rhc_variables(:)
+            real   (real_kind               ), intent(in) :: lhc_position (3)
+            real   (real_kind               ), intent(in) :: rhc_position (3)
+            integer(int_kind                ), intent(in) :: num_variables
+            real   (real_kind               )             :: gradient_variables(num_variables*3)
+        end function compute_interface
+    end interface
+end module abstract_face_gradient_calculator
